c4fa560a162ba6717a359f0236f19fda1d738840
Pierre-Chesne <pierrc@hotmail.com>
2023-12-07 15:36:22 +0100

    test beauty

--- app/aca_demo/setup.sh
+++ app/aca_demo/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-AZURE_RESOURCE_GROUP_ACA=rg_demo_aca
+AZURE_RESOURCE_GROUP_ACA=rg_demo_beauty_aca
 AZURE_RESOURCE_GROUP_ACR=rg_demo_container
 AZURE_LOCATION=westeurope
 ACR_NAME=peteracr007
@@ -10,27 +10,25 @@ REVISION_01=rev-01
 VERSION_2_APPLICATION=2.0.0
 REVISION_02=rev-02
 
-#az group create \
-#   --name $AZURE_RESOURCE_GROUP_ACA \
-#   --location $AZURE_LOCATION
-#
-#az acr show \
-#   --name $ACR_NAME \
-#   --resource-group $AZURE_RESOURCE_GROUP_ACR \
-#   --output table
-#
-#az containerapp env create \
-#   --name $ENVIRONMENT_NAME \
-#   --resource-group $AZURE_RESOURCE_GROUP_ACA \
-#   --location $AZURE_LOCATION \
-#   --logs-destination none
+az group create \
+   --name $AZURE_RESOURCE_GROUP_ACA \
+   --location $AZURE_LOCATION
 
+az acr show \
+   --name $ACR_NAME \
+   --resource-group $AZURE_RESOURCE_GROUP_ACR \
+   --output table
 
-#az acr build -t $ACR_NAME.azurecr.io/$APPLICATION:$VERSION_1_APPLICATION -r $ACR_NAME .
+az containerapp env create \
+   --name $ENVIRONMENT_NAME \
+   --resource-group $AZURE_RESOURCE_GROUP_ACA \
+   --location $AZURE_LOCATION \
+   --logs-destination none
+
+az acr build -t $ACR_NAME.azurecr.io/$APPLICATION:$VERSION_1_APPLICATION -r $ACR_NAME .
+#az acr build -t $ACR_NAME.azurecr.io/$APPLICATION:$VERSION_2_APPLICATION -r $ACR_NAME .
 
 REGISTRY_PASSWORD=$(az acr credential show --name $ACR_NAME -o tsv --query "passwords[0].value")
-
-
 
 az containerapp create \
   --name $APPLICATION \
